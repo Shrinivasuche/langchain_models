5818a788287b1b9804892b1c9b6671adf4ada035
Shrinivasuche <shrinivasuche@gmail.com>
2026-04-04 02:21:16 +0530

    implemented chatmodel with gemini api and hugging face api

---
+++ chatmodels/huggingface_llm_demo.py
@@ -0,0 +1,28 @@
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+import os
+# from langchain_huggingface.embeddings import HuggingFaceEndpointEmbeddings
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = HuggingFaceEndpoint(
+    repo_id="google/gemma-4-31B-it",
+    task="text-generation",
+    max_new_tokens=50,
+    temperature=0.7,
+    huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_API_TOKEN"),
+)
+chat = ChatHuggingFace(llm=model)
+
+conversation = [
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "What is the capital of India?"},
+]
+
+try:
+    response = chat.invoke(conversation)
+    print("\n--- AI Response ---")
+    print(response.content)
+except Exception as e:
+    print(f"Error: {e}")
