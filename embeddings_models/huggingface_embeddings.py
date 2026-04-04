9f88ae8b8702bf278620eeb7e98a7a96fc316dd2
Shrinivasuche <shrinivasuche@gmail.com>
2026-04-04 12:58:35 +0530

    created batch embedding for documents

--- embeddings_models/huggingface_embeddings.py
+++ embeddings_models/huggingface_embeddings.py
@@ -8,10 +8,17 @@ embeddings = HuggingFaceEndpointEmbeddings(
     model="sentence-transformers/all-MiniLM-L6-v2",
 )
 
+# single embedding
 text = "Delhi is the capital of India"
-
-# 3. Convert text to a vector (List of floats)
 vector = embeddings.embed_query(text)
 
 print(f"Vector Length: {len(vector)}")
 print(f"First 5 dimensions: {vector[:5]}")
+
+# batch embeddings
+batch_texts = ["Delhi is the capital of India", "Paris is the capital of France"]
+batch_vectors = embeddings.embed_documents(batch_texts)
+print(f"Batch Vector Length: {len(batch_vectors[0])}")
+print(f"First 5 dimensions of first vector: {batch_vectors[0][:5]} \n\n\n")
+print("Batch Vectors: \n", batch_vectors)
+
